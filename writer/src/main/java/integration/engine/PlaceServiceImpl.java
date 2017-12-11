b7ffd6c68cc67b6d1d93194eefd9c187678f8b4e
Pijush Das <pijushdas@gmail.com>
2017-12-11 11:43:05 +0100

    New Place Service

--- writer/src/main/java/integration/engine/PlaceServiceImpl.java
+++ writer/src/main/java/integration/engine/PlaceServiceImpl.java
@@ -3,6 +3,9 @@
  */
 package integration.engine;
 
+import java.io.File;
+import java.util.concurrent.BlockingQueue;
+
 import integration.CountryService;
 import integration.DistrictService;
 
@@ -13,6 +16,7 @@ import integration.DistrictService;
 public class PlaceServiceImpl implements CountryService, DistrictService {
     @Override
     public String getServiceName() {
+        BlockingQueue<File> x = null;
         return this.getClass().getSimpleName();
     }
 
